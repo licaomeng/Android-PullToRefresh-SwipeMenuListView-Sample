c71bae6706bc42d26eebec5b5aad295ccaa00177
unknown <lica@LICA6.asiapacific.hpqcorp.net>
2015-12-01 11:03:36 +0800

    deleted unused layout

--- gen/cn/swu/pulltorefreshswipemenulistviewsample/R.java
+++ gen/cn/swu/pulltorefreshswipemenulistviewsample/R.java
@@ -41,10 +41,9 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_list=0x7f030000;
-        public static final int activity_main=0x7f030001;
-        public static final int item_list_app=0x7f030002;
-        public static final int xlistview_footer=0x7f030003;
-        public static final int xlistview_header=0x7f030004;
+        public static final int item_list_app=0x7f030001;
+        public static final int xlistview_footer=0x7f030002;
+        public static final int xlistview_header=0x7f030003;
     }
     public static final class string {
         public static final int app_name=0x7f050000;
